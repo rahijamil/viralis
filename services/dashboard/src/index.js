60f8640f562b267ed561fbe46240e05d2deeb561
rahijamil <cs.mohammadrahi@gmail.com>
2026-03-11 19:49:08 +0600

    fix: express 5 wildcard routing in dashboard

--- services/dashboard/src/index.js
+++ services/dashboard/src/index.js
@@ -17,7 +17,7 @@ app.get("/api/summary", (req, res) => {
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
